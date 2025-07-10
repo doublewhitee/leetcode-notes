5fad393681afa6076b4e249a5ac517adebf4562e
doublewhitee <doublewhite233@gmail.com>
2025-07-11 01:20:15 +0800

    fix: 修改指定输出目录

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,7 +4,6 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/leetcode-notes/",
-  dest: "/src/.vuepress/dist",
   lang: "zh-CN",
   title: "Whitee",
   description: "Hello, I'm Whitee",
